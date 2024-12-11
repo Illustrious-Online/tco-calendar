4daa2aeba5c36401fc589ecc3a17da46eaac172c
Nicholas Bekeris <nbeke@winegard.com>
2024-12-10 21:17:01 -0600

    Initialize new nextjs app for TCO calendar

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://2c11bf4ec0a7b86892720b580b7843a6@o4507144038907904.ingest.us.sentry.io/4508447298813952",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
